ed03b9adadea86f87972c32e8673f645467233c1
ncampos92 <nlcampos@uc.cl>
2017-03-26 18:57:54 -0300

    piechart overall stats

--- src/AppBundle/Controller/StatsController.php
+++ src/AppBundle/Controller/StatsController.php
@@ -6,6 +6,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Ob\HighchartsBundle\Highcharts\Highchart;
 
 /**
  * Statistics controller.
@@ -39,11 +40,48 @@ class StatsController extends Controller
    */
   public function showAction($id)
   {
+    $cumple = 0;
+    $noCumple = 0;
+    $noAplica = 0;
+
     $em = $this->getDoctrine()->getManager();
     $appForm = $em->getRepository('AppBundle:AppForm')->find($id);
 
+    foreach ($appForm->getFormAnswers() as $formAnswer) {
+      foreach ($formAnswer->getQuestionAnswers() as $questionAnswer) {
+        switch ($questionAnswer->getValue()) {
+          case 'cu':
+            $cumple++;
+            break;
+          case 'nc':
+            $noCumple++;
+            break;
+          default:
+            $noAplica++;
+            break;
+        }
+      }
+    }
+
+    $ob = new Highchart();
+    $ob->chart->renderTo('piechart');
+    $ob->title->text('General');
+    $ob->plotOptions->pie(array(
+        'allowPointSelect'  => true,
+        'cursor'    => 'pointer',
+        'dataLabels'    => array('enabled' => false),
+        'showInLegend'  => true
+    ));
+    $data = array(
+        array('Cumple', $cumple),
+        array('No Cumple', $noCumple),
+        array('No Aplica', $noAplica),
+    );
+    $ob->series(array(array('type' => 'pie','name' => 'Total de Respuestas', 'data' => $data)));
+
     return $this->render('stats/show.html.twig', array(
       'appForm' => $appForm,
+      'chart' => $ob,
     ));
   }
 
